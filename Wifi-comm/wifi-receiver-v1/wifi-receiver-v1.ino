7544b5a64dd2e4842ca0b81e72ca4d72d61d0070
Richard Hoogenboom <richard.hoogenboom@belleron.net>
2018-02-16 13:05:17 +0100

    wifi examples

--- Wifi-comm/wifi-receiver-v1/wifi-receiver-v1.ino
+++ Wifi-comm/wifi-receiver-v1/wifi-receiver-v1.ino
@@ -1,58 +1,21 @@
-/*
-* Arduino Wireless Communication Tutorial
-*       Example 1 - Receiver Code
-*                
-* by Dejan Nedelkovski, www.HowToMechatronics.com
-* 
-* Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
-*/
-
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-
-#include "OSLController.h"
-
-
-RF24 radio(3, 4); // CE, CSN
-
-OSLController controller;
-
-//replace easy to guess default address "00001";
-//http://www.firewall.cx/cisco-technical-knowledgebase/cisco-wireless/828-cisco-wireless-wlan-keygen.html
+RF24 radio(7, 8); // CE, CSN
+//const byte address[6] = "00001";
 const byte address[6] = {0x66,0x68,0x7b,0x4a,0x63};   
-
 void setup() {
   Serial.begin(9600);
   radio.begin();
   radio.openReadingPipe(0, address);
   radio.setPALevel(RF24_PA_MIN);
   radio.startListening();
-  
-//  char input[32] = {'c','1','=','1','2','0','3',';','c','2','=','1','5','0','0',';','c','3','=','2','3','0','4',';','m','o','d','e','=','5',';'};
-//  Serial.println(controller.processSettings(input));
-//  char input2[32] = {'m','1','=','1',';','m','2','=','1',';','m','3','=','0',';','m','4','=','0',';'};
-//  Serial.println(controller.processSettings(input2));
-//  char input3[32] = {'m','5','=','0',';','m','6','=','0',';','m','7','=','0',';','m','8','=','0',';'};
-//  Serial.println(controller.processSettings(input3));
-
-  Serial.println(controller.printDebugInfo());
 }
-int teller = 0;
-
 void loop() {
-//  if (radio.available()) {
-//    char text[32] = "";
-//    radio.read(&text, sizeof(text));
-//    Serial.println(text);
-//  }
-//  teller++;
-//  if ((teller % 1000) == 0) {
-//    Serial.println(millis());
-//    teller = 0;
-//  }
-//  delay(5); 
-
-  receiveControllerInfo();
-
+  if (radio.available()) {
+    char text[32] = "";
+    radio.read(&text, sizeof(text));
+    Serial.println(text);
+    delay(500);
+  }
 }
--- Wifi-comm/wifi-transmitter-v1/wifi-transmitter-v1.ino
+++ Wifi-comm/wifi-transmitter-v1/wifi-transmitter-v1.ino
@@ -1,75 +1,18 @@
-/*
-* Arduino Wireless Communication Tutorial
-*     Example 1 - Transmitter Code
-*                
-* by Dejan Nedelkovski, www.HowToMechatronics.com
-* 
-* Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
-*/
-
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-
-#include <OSLController.h>
-
-OSLController controller;
-
-        //receiver items and model inputs
-        const byte ThrottleChannel_Pin =    21;                      // The Arduino pin connected to the throttle channel input - D21/pin43
-        const byte SteeringChannel_Pin =    20;                      // The Arduino pin connected to the steering channel input - D20/pin44
-        const byte Channel3_Pin        =    19;                      // The Arduino pin connected to the multichannel input - D19/pin45
-        const byte MixSteeringChannel_Pin = 18;                      // The Arduino pin connected to the steering channel for mixing the rear axles - D18/pin46
-        const byte PotMeter            =    A1;                      // The Arduino pin connected to the potentio-meter from the 5th wheel A1/pin96
-
-        //OSL board items
-        const byte GreenLED            =    A4;                      // The Arduino pin connected to the on-board Green LED A4/pin93
-        const byte RedLED              =    A5;                      // The Arduino pin connected to the on-board Red LED A5/pin92 
-        const byte SetupButton         =    A0;                      // The Arduino pin connected to the on-board push button A0/pin97 
-
-        const byte NFR_CSN             =    4;                       // The Arduino pin connected to the NFR CSN pin D4/pin1
-        const byte NFR_CE              =    3;                       // The Arduino pin connected to the NFR CE pin D3/pin7
-        
-        RF24 radio(NFR_CE, NFR_CSN); // NFR CE, CSN connections
-        //NFR pin connections:
-        //Arduino
-        //Pin   Pin Name            Mapped Pin Name       Connected To  Device
-        //1     PG5 ( OC0B )        Digital pin 4 (PWM)   CSN           NFR
-        //7     PE5 ( OC3C/INT5 )   Digital pin 3 (PWM)   CE            NFR
-        //11    GND GND GND NFR
-        //20    PB1 ( SCK/PCINT1 )  Digital pin 52 (SCK)  SCK           NFR
-        //21    PB2 ( MOSI/PCINT2 ) Digital pin 51 (MOSI) MOSI          NFR
-        //22    PB3 ( MISO/PCINT3 ) Digital pin 50 (MISO) MISO          NFR
-        const byte address[6] = {0x66,0x68,0x7b,0x4a,0x63};    
-
+RF24 radio(7, 8); // CE, CSN
+const byte address[6] = "00001";
 void setup() {
   Serial.begin(9600);
   radio.begin();
   radio.openWritingPipe(address);
   radio.setPALevel(RF24_PA_MIN);
   radio.stopListening();
-
-  char input[32] = {'c','1','=','1','2','0','3',';','c','2','=','1','5','0','0',';','c','3','=','2','3','0','4',';','m','o','d','e','=','5',';'};
-  Serial.println(controller.processSettings(input));
-  char input2[32] = {'m','1','=','1',';','m','2','=','1',';','m','3','=','0',';','m','4','=','0',';'};
-  Serial.println(controller.processSettings(input2));
-  char input3[32] = {'m','5','=','0',';','m','6','=','0',';','m','7','=','0',';','m','8','=','0',';'};
-  Serial.println(controller.processSettings(input3));  
-
 }
-char text[32];
-
 void loop() {
-  controller.getControllerInfo().toCharArray(text, 32);
+  const char text[] = "Hello richard";
   radio.write(&text, sizeof(text));
+  delay(1000);
   Serial.println(text);
-  delay(100);
-  controller.getLightsInfo1_4().toCharArray(text, 32);
-  radio.write(&text, sizeof(text));
-  Serial.println(text);
-  delay(100);
-  controller.getLightsInfo5_8().toCharArray(text, 32);
-  radio.write(&text, sizeof(text));
-  Serial.println(text);
-  delay(100);
 }
