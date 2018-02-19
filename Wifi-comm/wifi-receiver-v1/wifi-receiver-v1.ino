0fe3edef358e665a86902db25f797ec3c88fb364
Richard Hoogenboom <richard.hoogenboom@belleron.net>
2018-02-19 19:16:03 +0100

    transmission with timer

--- Wifi-comm/wifi-receiver-v1/wifi-receiver-v1.ino
+++ Wifi-comm/wifi-receiver-v1/wifi-receiver-v1.ino
@@ -1,21 +1,48 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+
 RF24 radio(7, 8); // CE, CSN
 //const byte address[6] = "00001";
 const byte address[6] = {0x66,0x68,0x7b,0x4a,0x63};   
+
+typedef struct
+{
+  uint8_t state;
+  uint8_t type;
+} OSLLight;
+
+typedef struct
+{
+  uint16_t controller1;
+//  uint16_t controller2;
+//  uint16_t controller3;
+  OSLLight lights[12];
+} OSLLightPacket;
+
+OSLLightPacket packet;
+
+bool useStructure = true;
+
 void setup() {
   Serial.begin(9600);
   radio.begin();
+  radio.setPALevel(RF24_PA_MIN); //RF24_PA_MIN = 0,RF24_PA_LOW, RF24_PA_HIGH, RF24_PA_MAX, RF24_PA_ERROR
+  radio.setDataRate(RF24_2MBPS); //RF24_1MBPS = 0, RF24_2MBPS, RF24_250KBPS
   radio.openReadingPipe(0, address);
-  radio.setPALevel(RF24_PA_MIN);
   radio.startListening();
 }
 void loop() {
   if (radio.available()) {
-    char text[32] = "";
-    radio.read(&text, sizeof(text));
-    Serial.println(text);
-    delay(500);
+    if (!useStructure) {    
+      char text[32] = "";    
+      radio.read(&text, sizeof(text));
+      Serial.println(text);
+    } 
+    else {
+      radio.read(&packet, sizeof(packet));
+      Serial.print("controller 1: ");    Serial.println(packet.controller1);
+      Serial.print("Light 1: ");     Serial.println(packet.lights[0].state);
+    }
   }
 }
---
+++ Wifi-comm/wifi-timer-transmitter-v1/wifi-timer-transmitter-v1.ino
@@ -0,0 +1,71 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <OSL_SimpleTimer.h>
+
+RF24 radio(3, 4); // CE, CSN
+const byte address[6] = {0x66,0x68,0x7b,0x4a,0x63}; 
+
+OSL_SimpleTimer timer; 
+#define transmissionInterval 250  //ms transmission interval
+unsigned int transmissionTimerID;
+bool startUp;
+
+typedef struct
+{
+  uint8_t state;
+  uint8_t type;
+} OSLLight;
+
+typedef struct
+{
+  uint16_t controller1;
+//  uint16_t controller2;
+//  uint16_t controller3;
+  OSLLight lights[12];
+} OSLLightPacket;
+
+OSLLightPacket packet;
+
+bool useStructure = true;
+  
+void setup() {
+  startUp = true;
+  Serial.begin(9600);
+  radio.begin();
+  radio.setPALevel(RF24_PA_MIN); //RF24_PA_MIN = 0,RF24_PA_LOW, RF24_PA_HIGH, RF24_PA_MAX, RF24_PA_ERROR
+  radio.setDataRate(RF24_2MBPS); //RF24_1MBPS = 0, RF24_2MBPS, RF24_250KBPS
+  radio.openWritingPipe(address);
+  radio.stopListening();
+
+
+  packet.controller1 = 1501;
+//  packet.controller2 = 1050;
+//  packet.controller3 = 2100;
+  packet.lights[0].state = 1;
+  packet.lights[0].type = 3;
+  packet.lights[0].state = 0;
+  packet.lights[0].type = 2;
+
+}
+
+void transmitControllerInfo() {
+  if (!useStructure) {
+    char text[32];
+    sprintf(text, "It is now: %d ms", millis());
+    radio.write(&text, sizeof(text));
+  }
+  else {
+    packet.controller1 = millis();
+    radio.write(&packet, sizeof(packet));
+  }
+}
+
+
+void loop() {
+  if (startUp) {
+    transmissionTimerID = timer.setInterval(transmissionInterval, transmitControllerInfo);
+    startUp = false;
+  }
+  timer.run();  
+}
